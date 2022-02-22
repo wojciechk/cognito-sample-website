a8fd7a1d5df138b66677400684cf2d434c7d427f
Wojciech Kaminski <wojciechka@gmail.com>
2022-02-22 12:03:18 +0100

    first commit

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_anncc77ut',
+        userPoolClientId: '4jfid81ubcn5vtrihrdc7rtii4',
+        region: 'e.g. us-east-2'
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
